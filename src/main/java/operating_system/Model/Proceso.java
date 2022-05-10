31aa2c32c550eb579dc40d8cd27ea35669111513
JahirGomez <86177245+JahirGomez@users.noreply.github.com>
2022-05-09 19:00:18 -0500

    Little mod ;)

--- src/main/java/operating_system/Model/Proceso.java
+++ src/main/java/operating_system/Model/Proceso.java
@@ -9,7 +9,7 @@ public class Proceso extends Thread {
     @Override
     public void run (){
         for (int i=0; i<5; i++){
-            System.out.println (this.getName() + ". " + i);
+            System.out.println ("Ejecutando " + this.getName());
         }
     }
 }
