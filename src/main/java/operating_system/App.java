00f06f7e2bbb0d36c9040e0c933c389dfc85d35c
JahirGomez <86177245+JahirGomez@users.noreply.github.com>
2022-05-10 23:19:38 -0500

    Update.

--- src/main/java/operating_system/App.java
+++ src/main/java/operating_system/App.java
@@ -4,34 +4,28 @@ import java.util.Scanner;
 import java.util.ArrayList;
 import operating_system.Model.Proceso;
 
-/**
- * Hello world!
- *
- */
 public class App 
-{
-    public static void main( String[] args )
-    {
+{ 
+    public static void main( String[] args ) {
+        int cantidad;
         ArrayList listaProcesos = new ArrayList<>();
         Scanner scanner = new Scanner(System.in);
-        int cantidad, tiempo;
         System.out.println("Ingrese el número de procesos a generar");
         cantidad = scanner.nextInt();
-        System.out.println("Ingrese el la ranura de tiempo en milisegundos");
-        tiempo = scanner.nextInt();
         
         for (int i=0; i<cantidad; i++){
-            Proceso p = new Proceso(i, tiempo);
-            p.start();
-            try{
-                p.sleep(tiempo/10);
-            }catch (InterruptedException e){
-                System.out.println("Error de" + p.getName() + " en " + e);
-            }
-            
+            int quantum, rafaga;
+            System.out.println("Ingrese el tiempo de CPU del proceso:");
+            rafaga = scanner.nextInt();
+            System.out.println("Ingrese el el quantum del proceso en milisegundos");
+            quantum = scanner.nextInt();
+            Proceso p = new Proceso(i, rafaga, quantum, cantidad);
             listaProcesos.add(p);
         }
 
+        for (int i=0; i<cantidad; i++){
+            ((Proceso)listaProcesos.get(i)).start();
+        }
 
     }
 }
--- src/main/java/operating_system/Model/Proceso.java
+++ src/main/java/operating_system/Model/Proceso.java
@@ -1,33 +1,95 @@
 package operating_system.Model;
 
 public class Proceso extends Thread {
-    private int slot;
+    private static int n;
+    private int rafaga, quantum, resto;
+    private String estado = "Listo";
 
-    public Proceso (int number, int n){
+    public Proceso (int number, int r, int q, int cantidad){
         super("P"+number);
-        this.slot=n;
+        this.rafaga=this.resto=r;
+        this.quantum=q;
+        Proceso.setN(cantidad);
     }
 
     @Override
     public void run (){
-        for (int i=0; i<5; i++){
-            System.out.println ("Ejecutando " + this.getName());
-            try{
-                sleep(this.slot/10);
-            }catch (InterruptedException e){
-                System.out.println("Error de" + this.getName() + " en " + e);
+        while (!this.estado.equals("Finalizado")){
+            if (resto>0 && resto>quantum){
+                System.out.println(this);
+                for (int j=0; j<quantum; j++){
+                    this.resto--;
+                    if (resto==0){
+                        break;
+                    }
+                }
+                
+                if (resto == 0){
+                    this.estado = "Finalizado";
+                    System.out.println(this);
+                    Proceso.setN(n-1);
+                    this.stop();
+                }else{
+                    this.estado = "En espera";
+                    System.out.println(this.getName() + " " +this.estado);
+                }
+                //this.dormir((Proceso.getN()-1)*quantum);
+            }else{
+                if (resto>0 && quantum !=0){
+                    //System.out.println(this);
+                    while (resto>0){
+                        this.resto--;
+                        if (resto==0){
+                            break;
+                        }
+                    }
+                    if (resto == 0 && quantum !=0){
+                        this.estado = "Finalizado";
+                        System.out.println(this);
+                        Proceso.setN(n-1);
+                        this.stop();
+                    }else{
+                        this.estado = "En espera";
+                        System.out.println(this.getName() + " " +this.estado);
+                    }
+                    //this.dormir((Proceso.getN()-1)*quantum);
+                }else{
+                    if (resto == 0 && quantum !=0){
+                        this.estado = "Finalizado";
+                        System.out.println(this);
+                        Proceso.setN(n-1);
+                        this.stop();
+                    }
+                }
             }
+            this.dormir((Proceso.getN()-1)*quantum);
         }
     }
 
 
     @Override
     public String toString() {
-        return "Ejecutando " + this.getName();
+        if (!estado.equals("Finalizado")){
+            return "Ejecutando " + this.getName();
+        }else{
+            return this.getName() +  " Finalizado";
+        }
     }
 
-
-    public void iniciar (){
-        this.start();
+    public void dormir(int a){
+        try{
+            this.sleep(a);
+        }catch (InterruptedException e){
+            System.out.println("Error de" + this.getName() + " en " + e);
+        }
     }
+
+    public static void setN (int a){
+        n = a;
+    }
+
+    public static int getN (){
+        return n;
+    }
+
 }
